c96b53202f0885911678d536b04dcbcff07b4308
mottox2 <y.mottox2@gmail.com>
2020-08-24 18:31:48 +0900

    feat: add animation with react-spring

--- src/GestureList.tsx
+++ src/GestureList.tsx
@@ -1,51 +1,69 @@
-import { relative } from 'path'
 import React, { useRef, useState } from 'react'
+import { useSprings, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
 import swap from 'lodash-move'
 
-const height = 40
+const height = 52
+
+const fn = (
+  order: number[],
+  down?: boolean,
+  originalIndex?: number,
+  currentIndex?: number,
+  y?: number,
+) => (index: number) => {
+  console.log({ order, down, originalIndex, currentIndex, y })
+  return {
+    y:
+      y && originalIndex === index && down
+        ? currentIndex! * height + y
+        : order.indexOf(index) * height,
+    immediate: originalIndex === index && down,
+  }
+}
 
 export const GestureList = () => {
-  const [items, setItems] = useState([1, 2, 5, 12])
+  const [items, setItems] = useState(['Home', 'Search', 'MyPage'])
+  const order = useRef(items.map((_, index) => index))
+  const [springs, setSprings] = useSprings(items.length, fn(order.current))
   const [draggingIndex, setIndex] = useState(-1)
   const draggingItem = useRef<HTMLLIElement>(null)
-  const currentIndex = useRef(0)
 
   const bind = useDrag((state) => {
-    console.log(state.movement)
-    if (state.first) {
-      setIndex(state.args[0])
-      currentIndex.current = draggingIndex
-      return
-    }
-    if (!draggingItem.current) return
-    const moveY = state.movement[1]
-    const nextY = draggingIndex * height + moveY
-    console.log(draggingItem.current, { draggingIndex, moveY, nextY })
+    const {
+      movement: [_, moveY],
+      down,
+      args: [originalIndex],
+    } = state
+    const currentIndex = order.current.indexOf(originalIndex)
+    const nextY = currentIndex * height + moveY
     const nextRow = Math.min(
       Math.max(Math.round(nextY / height), 0),
       items.length - 1,
     )
-
-    // if (currentIndex.current !== nextRow) {
-    //   setItems(swap(items, draggingIndex, nextRow))
-    //   draggingItem.current.style.top = ``
-    //   draggingItem.current.style.transition = '.1s transform'
-
-    //   setIndex(nextRow)
-    //   currentIndex.current = nextRow
-    // }
-
-    console.log(nextRow)
-    draggingItem.current.style.transition = 'none'
-    draggingItem.current.style.top = `${moveY}px`
-    if (state.last) {
-      draggingItem.current.style.top = ``
-      draggingItem.current.style.transition = '.1s transform'
-      setItems(swap(items, draggingIndex, nextRow))
-    }
+    const newOrder = swap(order.current, currentIndex, nextRow)
+    setSprings(fn(newOrder, down, draggingIndex, currentIndex, moveY))
+    console.log({ nextRow })
+    if (!down) order.current = newOrder
   })
 
+  return (
+    <ul className="gestureList">
+      {springs.map(({ y }, i) => (
+        <animated.li
+          className="gestureList-item"
+          key={i}
+          style={{ y, position: 'absolute', userSelect: 'none' }}>
+          <div className="gestureList-item-main">
+            ScreenName
+            <input placeholder="TabBar Title" type="text" value={items[i]} />
+          </div>
+          <span className="gestureList-item-handler" {...bind(i)}></span>
+        </animated.li>
+      ))}
+    </ul>
+  )
+  // @ts-ignore
   return (
     <ul>
       {items.map((i, index) => {
